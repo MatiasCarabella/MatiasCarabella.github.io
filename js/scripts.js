d30d6e988bf234078fe08c846c57097c76e37da7
MatiasCarabella <matias.carabella@outlook.com>
2021-11-08 03:30:00 -0300

    Continuing flow

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function nameHandler() {
     var name = document.getElementById("name").value;
     document.getElementById("home").style.display="none";
     if(gfNames.includes(name.toLowerCase())) {
-        document.getElementById("helloGf").style.display="block";
+        document.getElementById("helloDai").style.display="block";
     } else {
         document.getElementById("whoAreYou").style.display="block";
     }
@@ -13,4 +13,26 @@ function nameHandler() {
     document.getElementById("whoAreYou").style.display="none";
     document.getElementById("name").value = '';
     document.getElementById("home").style.display="block";
+  }
+
+  function getSize() {
+    switch(document.getElementById("nobioSize").value) {
+      case 'tall':
+        document.getElementById("helloDai").style.display="none";
+        document.getElementById("selectedTall").style.display="block";
+        break;
+      case 'smol':
+        document.getElementById("helloDai").style.display="none";
+        document.getElementById("selectedSmol").style.display="block";
+        break;
+      default:
+        alert("Por favor, seleccione la altura de su nobio");
+    }
+  }
+
+  function goToHelloDai() {
+    document.getElementById("helloDai").style.display="block";
+    document.getElementById("nobioSize").value = "";
+    document.getElementById("selectedTall").style.display="none";
+    document.getElementById("selectedSmol").style.display="none";
   }
